6f9a1cd1795d261bd4295621359397e30ac6de30
Ant <ryu4979@gmail.com>
2021-06-19 20:43:40 +0900

    fix: browser.close()の記述追加

--- main.js
+++ main.js
@@ -5,17 +5,22 @@ const fs = require("fs");
   const browser = await puppeteer.launch({
     headless: false,
     slowMo: 100
-  })
+  });
 
-  // NPBサイトのCookie
-  const content = fs.readFileSync("cookie.json");
-  const cookie = JSON.parse(content);
+  try {
+    // NPBサイトのCookie
+    const content = fs.readFileSync("cookie.json");
+    const cookie = JSON.parse(content);
 
-  const page = await browser.newPage();
-  await page.setCookie(...cookie);
+    const page = await browser.newPage();
+    await page.setCookie(...cookie);
 
-  await page.goto('https://npb.jp/allstar/ballot/members');
-  await Promise.all([page.waitForNavigation(), page.click('#btnSubmit')]);
-  await Promise.all([page.waitForNavigation(), page.click('#btnSubmit')]);
-
+    await page.goto('https://npb.jp/allstar/ballot/members');
+    await Promise.all([page.waitForNavigation(), page.click('#btnSubmit')]);
+    await Promise.all([page.waitForNavigation(), page.click('#btnSubmit')]);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await browser.close();
+  }
 })();
